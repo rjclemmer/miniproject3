5943f948d2dcd28e5f120bcf3bde2e5ed627fa77
Squirrely Rob <clemmer.robert.j@gmail.com>
2022-11-25 09:17:40 -0500

    ROCK SCISSORS PAPER SPOCK LIZARD!

--- script.js
+++ script.js
@@ -22,11 +22,16 @@ The computer's selection must be random to ensure a fair game.
 */
 
 // use for generating computer choice
+var generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", playGame);
+
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-var choice = ["R", "P", "S"]
+var choice = ["Rock", "Paper", "Scissors", "spock", "Lizard"]
 var playerChoice = ''
 var computerChoice = ''
 
@@ -45,7 +50,7 @@ var ties = 0
 
 function playGame() {
     // prompt user for input and store that input
-    playerChoice = prompt('Enter R, P, or S:')
+    playerChoice = prompt('Enter Rock, Paper, Scissors, Lizard, Spock:')
 
     // generate computer input
     computerChoice = choice[getRandomInt(3)] 
@@ -58,62 +63,116 @@ function playGame() {
 
     // compare user and computer inputs
     
-    if(playerChoice == "R") {
-        if(computerChoice =="R"){
+    if(playerChoice == "Rock") {
+        if(computerChoice =="Rock"){
         alert("Computer Chose Rock - You tie");
         ties += 1;
-    } else if (computerChoice == "P") {
-        alert("Computer Chose Paper - You lose");
-        losses += losses + 1;
-    } else {
-        alert("Computer Chose Scissors - You Win");
-        wins += wins + 1;
+    } else if (computerChoice == "Paper") {
+        alert("Computer Chose Paper - Paper Covers Rock - You lose");
+        losses += 1;
+    } else if (computerChoice == "Spock") {
+        alert("Computer Chose Spock - Spock Vaporizes Rock - You Lose");
+        losses += 1;
+    } else if (computerChoice == "Lizard"){
+        alert("Computer Chose Lizard - Rock Crushes Lizard - You Win");
+        wins += 1;
+    }
+    else{
+        alert("Computer Chose Scissors - Rock Crushes Scissors - You Win");
+        wins += 1;
     }
 }
 
-    if(playerChoice == "p") {
-        if(computerChoice =="P"){
+    if(playerChoice == "Paper") {
+        if(computerChoice =="Paper"){
         alert("Computer Chose Paper - You tie");
         ties += 1;
-    } else if (computerChoice == "S") {
-        alert("Computer Chose Scissors - You lose");
+    } else if (computerChoice == "Scissors") {
+        alert("Computer Chose Scissors - Scissors Cut Paper - You lose");
         losses += 1;
-    } else {
+    } else if(computerChoice == "Lizard") {
+        alert("Computer Chose Lizard - Lizard Eats Paper - You Lose");
+        losses += 1;
+    } else if (computerChoice == "Spock") {
+        alert("Computer Chose Spock - Paper Disproves Spock - You Win");
+        wins += 1;
+    }
+    else {
         alert("Computer Chose Rock - You Win");
         wins += 1;
     }
 }
 
-    if(playerChoice == "S") {
-        if(computerChoice =="S"){
-        alert("Computer Chose Scissors - You tie");
+    if(playerChoice == "Scissors") {
+        if(computerChoice =="Scissors"){
+        alert("Computer Chose Scissors - You Tie");
         ties += 1;
-    } else if (computerChoice == "R") {
-        alert("Computer Chose Rock - You lose");
+    } else if (computerChoice == "Rock") {
+        alert("Computer Chose Rock - Rock Crushes Scissors - You Lose");
         losses += 1;
-    } else {
-        alert("Computer Chose Paper - You Win");
+    } else if (computerChoice == "Spock") {
+        alert("Computer Chose Spock - Spock Smashes Scissors - You Lose");
+        losses += 1;
+    } else if (computerChoice == "Lizard") {
+        alert("Computer Chose Lizard - Scissors Decapitates Lizard - You Win");
+        wins += 1;
+    }
+    else {
+        alert("Computer Chose Paper - Scissors Cuts Paper - You Win");
         wins += 1;
     }
     }
 
+    // if spock
+    if(playerChoice == "Spock") {
+        if(computerChoice =="Spock"){
+        alert("Computer Chose Spock - You Tie");
+        ties += 1;
+    } else if (computerChoice == "Lizard") {
+        alert("Computer Chose Lizard - Lizard Poisons Spock - You Lose");
+        losses += 1;
+    } else if (computerChoice == "Paper") {
+        alert("Computer Chose Paper - Paper Disproves Spock - You Lose");
+        losses += 1;
+    } else if (computerChoice == "Rock") {
+        alert("Computer Chose Rock - Spock Vaporizes Rock - You Win");
+        wins += 1;
+    }
+    else {
+        alert("Computer Chose Scissors - Spock Smashes Scissors - You Win");
+        wins += 1;
+    }
+    }
+    // if lizard
+    if(playerChoice == "Lizard") {
+        if(computerChoice =="Lizard"){
+        alert("Computer Chose Lizard - You Tie");
+        ties += 1;
+    } else if (computerChoice == "Rock") {
+        alert("Computer Chose Rock - Rock Crushes Lizard - You Lose");
+        losses += 1;
+    } else if (computerChoice == "Scissors") {
+        alert("Computer Chose Scissors - Scissors Decapitates Lizard - You Lose");
+        losses += 1;
+    } else if (computerChoice == "Spock") {
+        alert("Computer Chose Spock - Lizard Poisons Spock - You Win");
+        wins += 1;
+    }
+    else {
+        alert("Computer Chose Paper - Lizard Eats Paper - You Win");
+        wins += 1;
+    }
     // determine who wins or if it is a tie
     // print out corresponding message with alert()
 // WIN/ LOSS
+    }
 
 
-
-alert("Wins: " + wins + " Losses: " + losses + " Ties: " + ties)
+    alert("Wins: " + wins + " Losses: " + losses + " Ties: " + ties)
     // update stats
 
     // ask user if they want to play again with confirm()
     var playAgain = confirm("Play Again?");
     if (playAgain) {
         playGame()
-    }
-}
-
-
-
-// run game for this first time
-playGame()
+    }}
